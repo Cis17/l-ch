fd8abf055f403abd2a221b427404e9c8942bf5f2
Cristian L. Muñoz H <cristian.munoz@avaldigitallabs.com>
2019-09-13 10:26:46 -0500

    add some dates logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DatePickerComponent } from './date-picker/date-picker.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { DatePickerComponent } from './date-picker/date-picker.component';
     DatePickerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'cc-date-picker',
   templateUrl: './date-picker.component.html',
-  styleUrls: ['./date-picker.component.css']
+  styleUrls: ['./date-picker.component.css'],
   encapsulation: ViewEncapsulation.Native
 })
 export class DatePickerComponent implements OnInit {
@@ -11,55 +11,89 @@ export class DatePickerComponent implements OnInit {
   private active: 'year' | 'month' | 'day' = undefined;
 
   @Input()
-  set min(minDate: string) {
+  set minDate(minDate: string) {
+    this.calculateYears();
     this._minDate = new Date(minDate);
   }
-  get min(): string {
-    return this._minDate.toDateString();
+
+  get minDate(): string {
+    return this._minDate.toString();
   }
+
   @Input()
-  set max(maxDate: string) {
+  set maxDate(maxDate: string) {
+    this.calculateYears();
     this._maxDate = new Date(maxDate);
   }
-  get max(): string {
-    return this._maxDate.toDateString();
+
+  get maxDate(): string {
+    return this._maxDate.toString();
   }
+
   @Input()
   set year(year: number) {
     this._year = year;
+    this.month = undefined;
   }
+
   get year() {
     return this._year;
   }
+
   @Input()
   set month(month: number) {
     this._month = month;
+    this.day = undefined;
   }
+
   get month() {
     return this._month;
   }
+
   @Input()
   set day(day: number) {
     this._day = day;
   }
+
   get day() {
     return this._day;
   }
 
-  _minDate = new Date('1900/01/01');
-  _maxDate = new Date('3000/12/31');
+  get years() {
+    return this._years;
+  }
 
-  _year: number;
-  _month: number;
-  _day: number;
+  get months() {
+    return Array.from({length: 12}, (v, k) => k + 1);
+  }
 
+  get days() {
+    if (!this.month) {
+      return [];
+    }
+    const days = new Date(this.year, this.month, 0).getDate();
+    return Array.from({length: days}, (v, k) => k + 1);
+  }
+
+  private _minDate = new Date('1900-01-01');
+  private _maxDate = new Date('3000-12-31');
+
+  private _years = [];
+  private _year: number;
+  private _month: number;
+  private _day: number;
 
 
   constructor() {
-    console.log(this.min, this._maxDate);
+    this.calculateYears();
   }
 
   ngOnInit() {
   }
 
+  private calculateYears() {
+    const minYear = this._minDate.getFullYear();
+    const maxYear = this._maxDate.getFullYear();
+    this._years = Array.from({length: maxYear - minYear + 1}, (v, k) => k + minYear);
+  }
 }
